50d3de14f698aa0ca42d1ccdd8d5fa2a7a9bdf92
bondan <bondanr@yahoo.com>
2026-04-20 22:59:10 -0700

    gitignore: exclude nested target/ and Cargo.lock

--- xezim-parser/target/release/build/quote-55b3a2f4f4bc83d4/build_script_build-55b3a2f4f4bc83d4.d
+++
@@ -1,5 +0,0 @@
-/home/bondan/agent/claude/repo/xezim/xezim-parser/target/release/build/quote-55b3a2f4f4bc83d4/build_script_build-55b3a2f4f4bc83d4.d: /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/bondan/agent/claude/repo/xezim/xezim-parser/target/release/build/quote-55b3a2f4f4bc83d4/build_script_build-55b3a2f4f4bc83d4: /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
--- xezim-parser/target/release/deps/unicode_ident-730a0328b14b1d39.d
+++
@@ -1,8 +0,0 @@
-/home/bondan/agent/claude/repo/xezim/xezim-parser/target/release/deps/unicode_ident-730a0328b14b1d39.d: /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/bondan/agent/claude/repo/xezim/xezim-parser/target/release/deps/libunicode_ident-730a0328b14b1d39.rlib: /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/bondan/agent/claude/repo/xezim/xezim-parser/target/release/deps/libunicode_ident-730a0328b14b1d39.rmeta: /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs /home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs
-
-/home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/lib.rs:
-/home/bondan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.24/src/tables.rs:
